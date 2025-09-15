2d05f8bfe541c3ccced3a2e7b8dc9abaca58a9a5
Guilherme <guilhermefogacamessias@alunos.utfpr.edu.br>
2025-09-15 17:51:24 -0300

    add: sessao 1-4

---
+++ src/main/java/org/example/FuncaoMatematica.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class FuncaoMatematica {
+    public static void main(String[] args) {
+        double a= Math.sqrt(9);
+        double b=Math.pow(2,2);
+        double c=Math.abs(-2);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
